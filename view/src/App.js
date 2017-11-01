39e797b858678dd80f520eea45f4da915936729d
Geeku <ggongpei@gmail.com>
2017-11-01 15:21:29 +0800

    react init

--- view/src/App.js
+++ view/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './img/logo.svg';
+import './style/index.scss';
+
+import Card from './components/Card';
+
+import bloginfo from './bloginfo.json';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="blog-header">
+          <img src={logo} className="blog-logo" alt="logo" />
+          <h1 className="blog-title">{bloginfo.name}</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          Main
+          <Card></Card>
+        </main>
+        <aside>
+          Aside
+        </aside>
+        <footer>
+          Footer
+        </footer>
       </div>
     );
   }
